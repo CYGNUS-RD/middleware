706f815ea02169a210d4dd91f7e41253ea8d285c
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2022-03-16 12:56:12 +0100

    equpmemt PMT+DB, ecc

---
+++ dev/event_receiver_db_v0.1.py
@@ -0,0 +1,262 @@
+"""
+A simple client that registers to receive events from midas.
+"""
+
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+from datetime import datetime
+import pre_reconstruction as pr
+import time
+
+
+import midas
+import midas.client
+
+import mysql.connector
+
+import sys
+
+# 
+sys.path.append('../')
+from cygno_conf import *
+#
+
+def makeped(ped_array):
+    #print(np.shape(ped_array))
+    
+    pedarr_fr = np.mean(ped_array, axis=0)
+    sigarr_fr = np.std(ped_array, axis=0)
+    #print(pedarr_fr[1:100,1])
+    return np.array(pedarr_fr), np.array(sigarr_fr)
+
+def run_reco(image,runnumber,ev_number,pedarr_fr, sigarr_fr):
+  
+   
+    #print ("New image arrived")
+    #print ("Analyzing Run%05d Image %04d"% (runnumber,ev_number))
+    ## Load image
+    arr = image
+    ## Include some reconstruction code here
+    #
+    t1 = time.time()
+    variables = pr.pre_reconstruction(arr,runnumber,ev_number,pedarr_fr,sigarr_fr,printtime=True)
+    t2 = time.time()
+
+    df = pr.create_pandas(variables)
+    return df
+
+def push_panda_table_sql(connection, table_name, df):
+    #mycursor = connection.cursor()
+    
+    mycursor=connection.cursor()
+    
+    mycursor.execute("SHOW TABLES LIKE '"+table_name+"'")
+    result = mycursor.fetchone()
+    if not result:
+        cols = "`,`".join([str(i) for i in df.columns.tolist()])
+        db_to_crete = "CREATE TABLE `"+table_name+"` ("+' '.join(["`"+x+"` REAL," for x in df.columns.tolist()])[:-1]+")"
+
+        print ("[Table %s created into SQL Server]".format(table_name))
+        #
+        # scommentare per eseguire
+        #
+        mycursor = connection.cursor()
+        mycursor.execute(db_to_crete)
+
+    cols = "`,`".join([str(i) for i in df.columns.tolist()])
+    #print(cols)
+    # Insert DataFrame recrds one by one.
+    for i,row in df.iterrows():
+        sql = "INSERT INTO `"+table_name+"` (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+        #print (sql, tuple(row.astype(str)))
+        mycursor.execute(sql, tuple(row.astype(str)))
+
+        # the connection is not autocommitted by default, so we must commit to save our changes
+        connection.commit()
+    
+    
+    
+#     mycursor.execute("SHOW TABLES LIKE '"+table_name+"'")
+#     result = mycursor.fetchone()
+#     if result:
+#         # there is a table named "tableName"
+#         print("bravo")
+#     else:
+#         print("pippa")
+#     return
+
+
+
+
+connection = mysql.connector.connect(
+  host=MYSQL_IP,
+  user=MYSQL_USER,
+  password=MYSQL_PASSWORD,
+  database=MYSQL_DATABASE,
+  port=MYSQL_PORT2
+)
+
+mycursor = connection.cursor()
+
+header_environment = [
+    'P0IIn0',
+    'P0IIn1',
+    'P0IIn2',
+    'P0IIn3',
+    'P0IIn4',
+    'P0IIn5',
+    'P0IIn6',
+    'P0IIn7',
+    'P0Calib',
+    'P1UIn0',
+    'P1UIn1',
+    'P1UIn2',
+    'P1UIn3',
+    'P1UIn4',
+    'P1UIn5',
+    'P1UIn6',
+    'P1UIn7',
+    'P1Calib',
+    'P3IIn0',
+    'P3IIn1',
+    'P3IIn2',
+    'P3IIn3',
+    'P3IIn4',
+    'P3IIn5',
+    'P3IIn6',
+    'P3IIn7',
+    'P3Calib']
+
+header_event = [
+    'timestamp',
+    'serial_number',
+    'event_id']
+
+header_event.extend(header_environment)
+
+if __name__ == "__main__":
+    # Create our client
+    client = midas.client.MidasClient("db_producer_v0.1")
+    
+    # Define which buffer we want to listen for events on (SYSTEM is the 
+    # main midas buffer).
+    buffer_handle = client.open_event_buffer("SYSTEM",None,1000000000)
+    
+    # Request events from this buffer that match certain criteria. In this
+    # case we will only be told about events with an "event ID" of 14.
+    # request_id = client.register_event_request(buffer_handle, event_id = 1)
+    request_id = client.register_event_request(buffer_handle)
+    
+    ped_array = []
+    ped_size = 10
+    ped_id = 0
+    ped_flag = True
+    
+    while True:
+        # If there's an event ready, `event` will contain a `midas.event.Event`
+        # object. If not, it will be None. If you want to block waiting for an
+        # event to arrive, you could set async_flag to False.
+        # event = client.receive_event(buffer_handle, async_flag=True)
+        event = client.receive_event(buffer_handle, async_flag=False)
+        if event.header.is_midas_internal_event():
+            print("Saw a special event")
+            continue
+        bank_names = ", ".join(b.name for b in event.banks.values())
+        print("Event # %s of type ID %s contains banks %s" % (event.header.serial_number, event.header.event_id, bank_names))
+        print("Received event with timestamp %s containing banks %s" % (event.header.timestamp, bank_names))
+        print("%s, banks %s" % (datetime.utcfromtimestamp(event.header.timestamp).strftime('%Y-%m-%d %H:%M:%S'), bank_names))
+        event_info = [event.header.timestamp, event.header.serial_number, event.header.event_id]
+
+        #if event is not None:
+        if bank_names=='CAM0':
+
+            shape = int(event.banks['CAM0'].size_bytes/2**12)
+            image = np.reshape(event.banks['CAM0'].data, (shape, shape))
+            
+            image = np.reshape(event.banks['CAM0'].data, (shape, shape))
+            if ped_flag:
+                
+                if ped_id >= ped_size:
+                    print("[Making Pedestal over {:d} images]".format(ped_size))
+                    pedarr_fr, sigarr_fr = makeped(ped_array)
+                    ped_flag = False
+                else:
+                    ped_array.append(image)
+                    ped_id+=1
+            else:
+                #print(image[1:100,1])
+                print("[Starting analysis Image {:d}]".format(event.header.serial_number))
+                runnumber = client.odb_get("/Runinfo/Run number")
+                table_name = "Run{:05d}".format(runnumber)
+                df = run_reco(image,runnumber,event.header.serial_number,pedarr_fr, sigarr_fr)
+                print("[Sending reco variables to SQL]")
+                push_panda_table_sql(connection,table_name, df)
+                #df.to_sql("Run10000", con=connection, if_exists='append', index_label='id')
+            
+        if bank_names=='DGH0':
+            
+            nboard = event.banks['DGH0'].data[0]
+            ich = 1
+            data_offset = 0
+            Waveform = []
+            for iboard in nboard:
+                name_board = event.banks['DGH0'].data[ich]
+                ich+=1
+                sample_number = event.banks['DGH0'].data[ich]
+                ich+=1
+                channels_number =  event.banks['DGH0'].data[ich]
+                ich+=1
+                number_events = event.banks['DGH0'].data[ich]
+                ich+=1
+                vertical_resulution = event.banks['DGH0'].data[ich]
+                ich+=1
+                sampling_rate = event.banks['DGH0'].data[ich]
+                for icahnnels in channels_number:
+                    ich+=1
+                    cannaels_offset[icahnnels] = event.banks['DGH0'].data[ich]
+                    
+                for ievent in number_events:
+                    for icahnnels in channels_number:
+                        start = data_offset 
+                        Waveform[ievent, ichannels] = event.banks['DIG0'].data[data_offset:data_offset+sample_number]
+                        data_offset += sample_number
+
+#             Waveform=[]
+#             for i in range(ndgtz):
+#                 Waveform.append(event.banks['DIG0'].data[i])
+#             x=np.arange(ndgtz)
+#            Waveform1 = event.banks['DIG0'].data[:ndgtz]
+
+            
+        if bank_names=='INPT':
+            print(datetime.utcfromtimestamp(event.header.timestamp).strftime('%Y-%m-%d %H:%M:%S'), 
+                  "event: "+str(event.header.serial_number))
+            print("  >>>  Entry in bank %s is %s" % (bank_names, event.banks['INPT'].data))
+            value = event_info + list(event.banks['INPT'].data)
+            de = pd.DataFrame(value, columns = header_environment)
+            table_name = "environment"
+            push_panda_table_sql(connection,table_name, de)
+#            sql = "INSERT INTO `environment` ("+" ".join(["`"+hed+"`," for hed in header_event])[:-1]+") VALUES ("+",".join([str(x) for x in value])+" )"
+            # print (sql)
+#            mycursor = connection.cursor()
+#            mycursor.execute(sql)
+#            connection.commit()
+#            print(mycursor.rowcount, "Record inserted successfully into Laptop table")
+            
+
+
+       # Talk to midas so it knows we're alive, or can kill us if the user
+        # pressed the "stop program" button.
+        client.communicate(10)
+        
+    # You don't have to cancel the event request manually (it will be done
+    # automatically when the program exits), but for completeness we're just
+    # showing that such a function exists.
+    client.deregister_event_request(buffer_handle, request_id)
+    # plt.show()
+    
+    # Disconnect from midas before we exit.
+    mycursor.close()
+    client.disconnect()
+    
