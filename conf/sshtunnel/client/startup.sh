a34821c4023bb781bfd47297b82e01dc1da5bcbd
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2023-12-01 17:25:16 +0100

    grafana update

--- conf/sshtunnel/client/startup.sh
+++ conf/sshtunnel/client/startup.sh
@@ -4,6 +4,6 @@ env
 d=`date +%Y-%M-%d\ %H:%M:%S`
 
 echo "$d STARTNG reverse SSHTUNNEL-->"
-chmod 600 /id_rsa
+chmod 600 /root/.ssh/id_rsa
 # cat /id_rsa
-ssh -o StrictHostKeyChecking=no -vnNTR ${REMOTE_PORT}:${LOCAL_APP_NAME}:${LOCAL_PORT} ${USER}@${REMOTE_IP} -i /id_rsa
+ssh -o StrictHostKeyChecking=no -vnNTC -L 0.0.0.0:${LOCAL_PORT}:${REMOTE_APP_NAME}:${REMOTE_PORT} ${USER}@${REMOTE_IP} -i /root/.ssh/id_rsa
