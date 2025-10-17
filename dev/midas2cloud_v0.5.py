f31cd098ecdfd070692b8fed5b022e9caff347a8
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2025-10-17 08:40:49 +0200

    m2c_0.5: check on DB insted of file;Check performed on DB instead of file. bag connection/configuration remouved

---
+++ dev/midas2cloud_v0.5.py
@@ -0,0 +1,296 @@
+#!/usr/bin/env python3
+#
+# G. Mazzitelli 2022
+# versione DAQ LNGS/LNF per midas file2cloud
+#
+# RUCIO upload
+#
+
+def storeStatus(filen, size, status, result, outfile):
+    from cygno import cmd
+    stchar = ' '.join([filen, str(size), str(status), str(result)])
+    cmd.append2file(stchar, outfile)
+    return stchar
+
+def replica_sql_value(connection, connection_remote, table_name, row_element, row_element_condition, verbose=False):
+    import datetime
+    sql = "SELECT * FROM `"+table_name+"` WHERE `"+row_element+"` = "+row_element_condition+";"
+    #SELECT * FROM `Runlog` WHERE `run_number` = 1024;
+    if verbose: print(sql)
+    #try:
+    # read from local
+    mycursor = connection.cursor()
+    mycursor.execute(sql)
+    row = mycursor.fetchone()
+
+    row_list = []
+
+    for i,data in enumerate(row):
+        if isinstance(data, datetime.datetime):
+            data = data.strftime('%Y-%m-%d %H:%M:%S')
+        row_list.append(data)
+    row = tuple(row_list)
+
+    cols = mycursor.column_names
+
+    cols = "`,`".join([str(i) for i in list(cols)])
+
+    mycursor.close()
+    #except:
+    #    return 0    
+    #sql = "INSERT INTO `{}` (`" +cols+ "`) VALUES {}".format(table_name, row)
+    sql = "INSERT INTO `"+table_name+"` (`" +cols+ "`) VALUES "+str(row)
+    if verbose: print(sql)
+    try:
+        # replica on remote
+        mycursor = connection_remote.cursor()
+        #sql = "INSERT INTO `"+table_name+"` (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+        mycursor.execute(sql)
+        #mycursor.execute(sql, row.astype(str))
+        connection_remote.commit()
+        mycursor.close()
+        
+        if verbose: print(mycursor.rowcount, "SQL Update done")
+
+        return row[0]
+    except:
+        return 0
+
+def daq_sql_connection_local(verbose=False):
+    import mysql.connector
+    import os
+    try:
+        connection = mysql.connector.connect(
+          host='localhost',
+          user=os.environ['MYSQL_USER'],
+          password=os.environ['MYSQL_PASSWORD'],
+          database=os.environ['MYSQL_DATABASE'],
+          port=3306
+        )
+        if verbose: print(connection)
+        return connection
+    except:
+        return False
+    
+    
+def main():
+    #
+    from optparse import OptionParser
+    import os,sys
+    import datetime
+    import numpy as np
+    import cygno as cy
+    import time
+    import midas
+    import midas.client
+    import subprocess
+    #
+    # deault parser value
+    #
+    version  ='s3v4'
+
+    BARI = 1
+    session  ='infncloud-wlcg'
+    bucket   = 'cygno-data'
+    
+    TAG         = os.environ['TAG']
+    INAPATH     = os.environ['INAPATH']# '/data01/data/'
+    DAQPATH     = os.environ['DAQPATH']# '/home/standard/daq/'
+    max_tries   = 5
+    #to = 'giovanni.mazzitelli@lnf.infn.it, davide.pinci@roma1.infn.it, francesco.renga@roma1.infn.it'
+    to = ''
+
+    parser = OptionParser(usage='usage: %prog [-t [{:s}] -i [{:s}]  -d [{:s}] -n [{:d}] -b [{:d}] rv]\n'.format(TAG, INAPATH,DAQPATH, max_tries,BARI))
+    parser.add_option('-t','--tag', dest='tag', type='string', default=TAG, help='tag where dir for data')
+    parser.add_option('-i','--inpath', dest='inpath', type='string', default=INAPATH, help='PATH to raw data')
+    parser.add_option('-d','--daqpath', dest='daqpath', type='string', default=DAQPATH, help='DAQ to raw data')
+    parser.add_option('-n','--tries', dest='tries', type=int, default=max_tries, help='# of retry upload temptative')
+    parser.add_option('-v','--verbose', dest='verbose', action="store_true", default=False, help='verbose output')
+    parser.add_option('-m','--mail', dest='mail', type='string', default=to, help='mail allert')
+    (options, args) = parser.parse_args()
+
+    max_tries   = options.tries
+    verbose     = options.verbose
+    to          = options.mail
+    tag         = options.tag
+     
+    INAPATH    = options.inpath
+    DAQPATH    = options.daqpath
+
+    STOROUT    = DAQPATH+"online/log/daq_stored.log"
+    FILELOK    = INAPATH+"analized.lok"
+    newupoload = []
+    print('{:s} midas2cloud started'.format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+    
+    client = midas.client.MidasClient("midas2cloud")
+    
+    compressing_files = 0
+    
+    key = tag+'/'
+
+    
+#    GB = 1024 ** 3
+#    config = TransferConfig(multipart_threshold=5*GB)
+
+    
+    connection = daq_sql_connection_local(verbose)#cy.daq_sql_cennection(verbose)
+    if not connection:
+        print('{:s} ERROR: Sql connetion'.format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+        sys.exit(1)
+    
+    while True:
+        # loop on midas (ecape on midas events)
+
+        try:
+            newupoload = []
+            # compress statment
+#            file_in_dir=sorted(os.listdir(INAPATH))
+
+            files = (
+               entry for entry in os.scandir(INAPATH)
+               if entry.is_file() and entry.name.startswith("run") and entry.name.endswith(".mid.gz")
+            )
+
+
+            # Ordinamento per nome e prendo solo gli ultimi 100
+            last_100 = sorted(files, key=lambda e: e.name)[-100:]
+
+            file_in_dir = [f.name for f in last_100]
+
+            if verbose : print ("Start DAQ backup at: ", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            if verbose : print ("File in dir %d" % np.size(file_in_dir))
+
+            ipload = 1 # index of file uploaded
+
+            # upload statment
+            for i, filename in enumerate(file_in_dir):
+                # questo if va sostituito con il check sul DB ABANDONADO IL LISI SUL FILE CHE E' LENTO
+#                if ('run' in str(filename))   and \
+#                ('.mid.gz' in str(filename))  and \
+#                (not (str(filename).split('.gz')[0] in file_in_dir)) \
+#                and (not filename.startswith('.')):
+
+                    runN = int(filename.split('run')[-1].split('.mid.gz')[0])
+
+                    # sostituisce il grep su file di cui sotto
+                    sqlFuleStatus=cy.daq_read_runlog_replica_status(connection, runN, "cloud") & cy.daq_read_runlog_replica_status(connection, runN, "tape")
+                    if sqlFuleStatus != 1 or cy.daq_read_runlog_replica_status(connection, runN, storage="local", verbose=verbose)==0:
+			
+
+#                    if cy.cmd.grep_file(filename, STOROUT) == "" or \
+#                    cy.daq_read_runlog_replica_status(connection, runN, storage="local", verbose=verbose)==0: 
+                        # end grep
+                        dtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                        print('{:s} Udatentig matadata for filer file: {:s}'.format(dtime, filename))
+
+                        filesize = os.path.getsize(INAPATH+filename)
+                        md5sum = cy.cmd.file_md5sum(INAPATH+filename)
+
+                        # upadete sql with new file local status
+                        cy.daq_update_runlog_replica_status(connection,
+                                                            runN, storage="local",
+                                                            status=1, verbose=verbose)
+
+                        cy.daq_update_runlog_replica_checksum(connection, runN, 
+                                                              md5sum, verbose=verbose)
+                        cy.daq_update_runlog_replica_size(connection, runN, 
+                                                              filesize, verbose=verbose)
+
+
+                        print('{:s} Transferring file: {:s}'.format(dtime, filename))
+                        if compressing_files > 0:
+                            compressing_files -= 1
+                        current_try = 0
+                        status = False 
+                        while(not status):   
+                            if verbose: print(INAPATH+filename,tag, bucket, session, verbose, filesize)
+                            if filesize < 5400000000: # ~ 5 GB
+                                dtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                                print('{:s} Uploading file: {:s}'.format(dtime, filename))
+
+                                   # remeber config sile in /home/.rucio.cfg
+                                docker_command = [ "docker", "run", "--rm",
+                                                      "-v", "/home/.rucio.cfg:/home/.rucio.cfg",
+                                                      "-v", INAPATH+filename+":/app/"+filename,
+                                                      "gmazzitelli/rucio-uploader:v0.2",  # nome dell'immagine Docker
+                                                      "--file", "/app/"+filename,
+                                                      "--bucket", bucket,
+                                                      "--did_name", key+filename,
+                                                      "--upload_rse", "CNAF_USERDISK",
+                                                      "--transfer_rse", "T1_USERTAPE",
+                                                      "--account", "rucio-daq"
+                                                      ]
+                                if verbose: print(docker_command)
+                                result = subprocess.run(docker_command, capture_output=True, text=True)
+                                exit_code = result.returncode
+
+				#| Exit Code | Meaning                                            |
+				#| --------- | -------------------------------------------------- |
+				#| 0         | Upload and replica created (or both already exist) |
+				#| 1         | File already uploaded, replica just created        |
+				#| 2         | Upload failed                                      |
+				#| 3         | Upload done, replica failed                        |
+				#| 4         | Client configuration error                         |
+
+
+                                if (exit_code==0 or exit_code==1):
+
+                                    if verbose: print('[DEBUG] Exit code: '+str(exit_code))
+
+                                    cy.daq_update_runlog_replica_status(connection, 
+                                                                            runN, storage="cloud", 
+                                                                            status=1, verbose=verbose)
+                                    cy.daq_update_runlog_replica_status(connection, runN, 
+                                                        storage="tape", status=1, verbose=verbose)
+                                    cy.daq_update_runlog_replica_tag(connection, runN, 
+                                                                         TAG=tag, verbose=verbose)
+
+                                    print('{:s} Upload done: {:s}'.format(dtime, filename))
+                                    status = True
+                                    newupoload.append(storeStatus(filename, filesize, status, exit_code, STOROUT))
+
+                                else:
+                                    current_try = current_try+1
+                                    print('{:s} WARNING: Uploading file: {:s}, try: {:s}, code error: {:s}'.format(dtime, filename, str(current_try), str(exit_code)))
+                                    cy.daq_update_runlog_replica_status(connection, 
+                                                                            runN, storage="cloud", 
+                                                                            status=-2, verbose=verbose)
+                                    cy.daq_update_runlog_replica_status(connection, runN, 
+                                                        storage="tape", status=-2, verbose=verbose)
+                                    if current_try==max_tries:
+                                        print('{:s} ERROR: Max try number reached'.format(dtime))
+                                        print("DEBUG result: ",result.returncode, result.stdout, result.stderr)
+                                        status = True
+                            else:
+                                storeStatus(filename, filesize, status, 0,  STOROUT)
+                                print('{:s} ERROR: File too large, not uploaded: {:s}'.format(dtime,filename))
+                                status=True
+
+            if len(newupoload)==0:
+                dtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print('{:s} WARNING: No new file uploaded'.format(dtime))
+
+            if to != '':
+                when = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+                subject = "latest news fro DAQ!"
+                if len(newupoload)>0:
+                    body = 'last files uploaded at '+when+': '+str([s for s in newupoload])
+                    # allert_mail(subject, body, to, verbose)
+                else:
+                    body = 'WARNING: no new uploaded files at '+when
+                    allert_mail(subject, body, to, verbose)
+            if verbose : print("\n Finished!!")
+
+            client.communicate(10)
+            time.sleep(10)
+
+        except KeyboardInterrupt:
+            client.deregister_event_request(buffer_handle, request_id)
+            client.disconnect()
+            print ("\nBye, bye...")
+            sys.exit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ dev/s3_put-botosts.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+import boto3
+from botocore.client import Config
+from boto3.s3.transfer import TransferConfig
+import os
+
+from optparse import OptionParser
+
+urls = ["https://swift.recas.ba.infn.it/", "https://minio.cloud.infn.it/", "https://s3.cr.cnaf.infn.it:7480/" ]
+
+parser = OptionParser(usage='usage: %prog\t filename -t tag -b bucket')
+parser.add_option('-v','--verbose', dest='verbose', action="store_true", default=False, help='verbose output;');
+parser.add_option('-b','--bucket', dest='bucket', type='string', default='cygno-analysis', help='tag');
+parser.add_option('-t','--tag', dest='tag', type='string', default='', help='tag');
+(options, args) = parser.parse_args()
+verbose = options.verbose
+if len(args)<1:
+    print("filename?")
+else:
+    filename = args[0]
+#print("ENDPOINTS")
+#for i, url in enumerate(urls):
+#    print("{:d}] {:s}".format(i, url))
+#S3_ENDPOINT = urls[int(input("ENDPOINT: 0..2? "))]
+
+S3_ENDPOINT=urls[2]
+print("endpoint: ", S3_ENDPOINT)
+
+def my_creds(url, verbose):
+
+    with open("/tmp/token","r") as f:
+        IAM_TOKEN = f.read().strip()
+    
+    sts_client = boto3.client('sts',
+                              endpoint_url=url,
+                              region_name='oidc')
+    
+    
+    
+    response = sts_client.assume_role_with_web_identity(
+            RoleArn="arn:aws:iam::cygno:role/IAMaccess",
+            RoleSessionName='Bob',
+            DurationSeconds=3600,
+            WebIdentityToken=IAM_TOKEN)
+    
+
+    if verbose: 
+        print(f"{response['Credentials']['AccessKeyId']=}\n{response['Credentials']['SecretAccessKey']=}")
+    return response
+
+def my_client(credentials, url, verbose):
+#    config = Config(signature_version='s3v4', s3={'addressing_style': 'path'})
+#    config = Config(signature_version='s3v4')
+    client = boto3.client('s3',
+                            aws_access_key_id = credentials['Credentials']['AccessKeyId'],
+                            aws_secret_access_key = credentials['Credentials']['SecretAccessKey'],
+                            aws_session_token = credentials['Credentials']['SessionToken'],
+                            endpoint_url=url,
+                            region_name='oidc')
+    return client
+
+if S3_ENDPOINT=="https://swift.recas.ba.infn.it/":
+    credentials = boto3.session.Session(
+        aws_access_key_id=os.environ.get('BA_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.environ.get('BA_SECRET_ACCESS_KEY')
+    )
+    s3 = credentials.client('s3', endpoint_url=urls[0],
+                            config=boto3.session.Config(signature_version='s3v4'),verify=True)
+else:
+    credentials = my_creds(S3_ENDPOINT, verbose)
+    s3 = my_client(credentials, S3_ENDPOINT, verbose)
+    
+GB = 1024 ** 3
+config = TransferConfig(multipart_threshold=5*GB)
+
+key = filename.split('/')[-1]
+key = options.tag+'/'+key
+s3.upload_file(filename, Bucket=options.bucket, Key=key, Config=config)
