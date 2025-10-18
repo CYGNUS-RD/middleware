a0884bba127c7496a49d737cfea8b0a2a4a37645
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2025-10-18 18:43:00 +0200

    boto backupper 2.0; new mariadb docker-compose with profile: grafana/sql; update grafana docker-compse

--- conf/start_vm.sh
+++ conf/start_vm.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 if [ "${VM_SERVICE}" = "sqlLNGS" ]; then
-   cd ./tape
-   docker compose up -d 
+#   cd ./tape
+#   docker compose up -d 
    cd ../mariadb
-   docker compose up -d 
-   cd ../analyzer
-   docker compose up -d 
+   docker compose  up -d 
+# to restart only when running
+#   cd ../analyzer
+#   docker compose up -d 
 elif [ "${VM_SERVICE}" = "GRAFANA" ]; then
    cd ./grafana
    docker compose up -d 
    cd ../mariadb
-   docker compose up -d 
+   docker compose --profile grafana up -d
+#   docker compose -f docker-compose_lnf_mango.yaml up -d 
 elif [ "${VM_SERVICE}" = "SENTINEL" ]; then
    cd ./sentinel
    docker compose up -d 
