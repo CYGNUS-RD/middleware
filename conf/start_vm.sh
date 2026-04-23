f3814db42679627e4bd6d0009fc12501476874f9
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2026-04-23 10:13:44 +0200

    Update start_vm.sh

--- conf/start_vm.sh
+++ conf/start_vm.sh
@@ -2,7 +2,7 @@
 if [ "${VM_SERVICE}" = "sqlLNGS" ]; then
 #   cd ./tape
 #   docker compose up -d 
-   cd ../mariadb
+   cd ./mariadb
    docker compose  up -d 
 # to restart only when running
 #   cd ../analyzer
@@ -10,7 +10,7 @@ if [ "${VM_SERVICE}" = "sqlLNGS" ]; then
 elif [ "${VM_SERVICE}" = "GRAFANA" ]; then
    cd ./grafana
    docker compose up -d 
-   cd ../mariadb
+   cd ./mariadb
    docker compose --profile grafana up -d
 #   docker compose -f docker-compose_lnf_mango.yaml up -d 
 elif [ "${VM_SERVICE}" = "SENTINEL" ]; then
